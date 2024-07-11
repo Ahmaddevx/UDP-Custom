853a512310bc727aaba64fdfaf7cce49adabdc26
Ahmaddevx <ahmaddevx@gmail.com>
2024-07-11 17:51:16 +0530

    ok

--- install.sh
+++ install.sh
@@ -61,8 +61,8 @@ else
     # [change timezone to UTC +0]
   echo ""
   echo " ⇢ Change timezone to UTC +0"
-  echo " ⇢ for Africa/Accra [GH] GMT +7:00"
-  ln -fs /usr/share/zoneinfo/Africa/Accra /etc/localtime
+  echo " ⇢ for Jakarta/Accra [GH] GMT +7:00"
+  ln -fs /usr/share/zoneinfo/Jakarta/Accra /etc/localtime
   sleep 3
 
   # [+clean up+]
